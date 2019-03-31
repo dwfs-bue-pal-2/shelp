a0f85713b472c440e72b91db634b7756216e36b2
Florencia <florenciamendez93@gmail.com>
2019-03-31 19:39:44 -0300

    Home page with modals

---
+++ client/controllers/controller-form.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+
+
+    $('#send').on('click', function(e) {
+        e.preventDefault();
+
+        var name = $("#name").val();
+        var description = $("#comments").val();
+        var address = $("#direccion").val();
+        var lat = $("#direccion").data('lat');
+        var lon = $("#direccion").data('lon');
+        var phone = $("#telefono").val();
+        var type = $("#dropdown").val();
+        var hours = $("#horario").val();
+        var img = $("#file").val();
+
+
+        if (name && description && address && type && phone && hours) {
+
+            $.post("http://localhost:3000/locales", {
+                    name: name,
+                    description: description,
+                    address: address,
+                    lat: lat,
+                    lon: lon,
+                    phone: phone,
+                    type: type,
+                    hours: hours,
+                    img: img
+                },
+                function(data, status) {
+
+                    console.log(" Status: " + status);
+                    if (status == 'success') {
+                        alert('El local ' + name + ' fue dado de alta con exito!');
+                        clearForm();
+                    }
+                });
+        } else {
+            alert('Debe completar todos los campos');
+        }
+    });
+
+    $('#cancel').on('click', function(e) {
+        e.preventDefault();
+        clearForm();
+        console.log(rating);
+    });
+
+    function clearForm() {
+        $("#alta-locales-form").find("input, textarea").val("");
+    };
+});
