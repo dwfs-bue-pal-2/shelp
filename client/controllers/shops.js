09eeeceda1e616d2751a4dafa388c364588d5648
Jose Ignacio Di Marco <jose.dimarco@globant.com>
2019-04-01 16:00:23 -0300

    map view with markers

--- client/controllers/shops.js
+++ client/controllers/shops.js
@@ -1,22 +1,23 @@
 $(document).ready(() => {
     getShop = () => {
-        $.ajax({   
+        $.ajax({
             method: "GET",
-            url: "http://private-29c6c-federicoolivarez.apiary-mock.com/locales",
-            success: function(data){
-               renderList(data);
-               console.log(data);
+            url: "http://localhost:8008/locales/",
+            success: function (data) {
+                renderList(JSON.parse(data));
+                console.log(JSON.parse(data));
             }
         });
     }
 
-    $(document).ajaxError(( event, request, settings) => {
+    $(document).ajaxError((event, request, settings) => {
         console.log('Error requesting page: ' + settings.url);
     });
 
     renderList = (data) => {
         data.forEach(shop => {
             addShopToList(shop);
+            markShops(shop);
         });
 
     }
@@ -32,17 +33,51 @@ $(document).ready(() => {
 
         getScore($modal, data.rating);
 
-        $('#shops .container').append($modal);  
+        $('#shops .container').append($modal);
     }
 
-    getScore = ($modal, rating) =>{
-        for(let i = 0 ; i< rating; i++){
+    getScore = ($modal, rating) => {
+        for (let i = 0; i < rating; i++) {
             $modal.find(".stars").append('<i class="fa fa-star"></i>');
         }
 
-        for(let i = 0 ; i< 5 - rating ; i++){
+        for (let i = 0; i < 5 - rating; i++) {
             $modal.find(".stars").append('<i class="far fa-star"></i>');
         }
+    }
+
+    markShops = (data) => {
+        let latLong = {
+            lat: data.lat,
+            lng: data.lon
+        }
+
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latLong),
+            label: data.name,
+            id: data.id,
+            map: map,
+        })
+
+        marker.addListener('click', function () {
+            let $modal = $("#modal-row").clone(true);
+
+            $modal.removeAttr("id");
+            $modal.find('#shop-name').text(data[this.get('id')].name);
+            $modal.find('#phone').text(data[this.get('id')].phone);
+            $modal.find('#address').text(data[this.get('id')].address);
+
+            let modal = $modal.html();
+
+            let string = modal.toString();
+            console.log(string);
+
+            let infowindow = new google.maps.InfoWindow({
+                content: string
+            });
+
+            infowindow.open(map, marker);
+        });
     }
 
     getShop();
