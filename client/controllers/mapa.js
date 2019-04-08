407863a9caf309cc9832e2f4635b7489d1a47796
Jose Ignacio Di Marco <jose.dimarco@globant.com>
2019-04-08 13:59:01 -0300

    current location

--- client/controllers/mapa.js
+++ client/controllers/mapa.js
@@ -18,8 +18,27 @@ function inicializarMapa() {
   	var input = document.getElementById('direccion');
     var searchBox = new google.maps.places.SearchBox(input);
 
-    
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+				var pos = {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
 
+				infoWindow = new google.maps.InfoWindow;
+
+				infoWindow.setPosition(pos);
+				infoWindow.setContent('Ubicacion actual');
+				infoWindow.open(map);
+				map.setCenter(pos);
+			}, function() {
+				handleLocationError(true, infoWindow, map.getCenter());
+			});
+		} else {
+			// Browser doesn't support Geolocation
+			handleLocationError(false, infoWindow, map.getCenter());
+			console.log("errrr");
+		}       
        
 }
 
