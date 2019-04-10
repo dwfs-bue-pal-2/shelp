18673c6fad29fe9c4822dc49c8fe83925912c959
ttSpeaker <js.serruya@gmail.com>
2019-04-09 21:53:56 -0300

    Comit para pasarle a Cesar

--- client/controllers/controller-form.js
+++ client/controllers/controller-form.js
@@ -11,48 +11,52 @@ $(document).ready(function () {
         e.preventDefault();
         getLatLong($("#direccion").val());
     });
-
+    
     $('#cancel').on('click', (e) => { 
         e.preventDefault();
         clearForm();
         $("#modal-form")[0].style.display = "none";
     });
-
-
+    
+    
 });
 
 function clearForm() {
     $("#alta-locales-form").find("input, textarea").val("");
 };
 
-function saveShop(lat, lon) {
-    let shop = {
-        name: $("#name").val(),
-        desc: $("#comments").val(),
-        address: $("#direccion").val(),
-        lat: lat,
-        lon: lon,
-        phone: $("#telefono").val(),
-        type: $("#dropdown").val(),
-        hours: $("#horario").val(),
-        img: $("#file").val()
-    }
 
-    $.post(server, {
-        name: shop.name,
-        description: shop.desc,
-        address: shop.address,
-        lat: shop.lat,
-        lon: shop.lon,
-        phone: shop.phone,
-        type: shop.type,
-        hours: shop.hours,
-        img: shop.img
-    }, (data, status) => {
-        console.log(`Status: ${status}`);
-        if (status === 'success') {
-            alert(`El local ${name} fue dado de alta con exito!`);
-            clearForm();
-        }
-    });     
+
+function saveShop(lat, lon) {
+    var shop = new FormData();
+    shop.append('name', $("#name").val());
+    shop.append('addres', $("#direccion").val());
+    shop.append('lat', $("#lat").val());
+    shop.append('lon', $("#lon").val());
+    shop.append('phone', $("#telefono").val());
+    shop.append('hours', $("#horario").val());
+    shop.append('shopImage', $("#file").val());
+
+    var settings = {
+        "url": "http://localhost:3000/locales",
+        "method": "POST",
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "processData": false,
+        "contentType": false,
+        "mimeType": "multipart/form-data",
+        "data": form
+      }
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+    // $.post(server, shop, (data, status) => {
+    //     console.log(`Status: ${status}`);
+    //     if (status === 'success') {
+    //         alert(`El local ${name} fue dado de alta con exito!`);
+    //         clearForm();
+    //     }
+    // });     
 };
