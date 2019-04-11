248475b4183e435b34ae8b72a42b98e7b1cc2069
ttSpeaker <js.serruya@gmail.com>
2019-04-11 19:09:35 -0300

    nothing to commit

--- client/controllers/controller-form.js
+++ client/controllers/controller-form.js
@@ -1,88 +1,93 @@
 const server = "http://localhost:3000/locales";
 
-$(document).ready(function () {
-    getShopList(markShops);
+$(document).ready(function() {
+  getShopList(markShops);
 
-    $("form").keypress((e) => {
-        if (e.which === 13) {
-            return false;
-        }
-    });
+  $("form").keypress(e => {
+    if (e.which === 13) {
+      return false;
+    }
+  });
 
-    $('#send').on('click', (e) => {
-        e.preventDefault();
-        getLatLong($("#direccion").val());
-    });
+  $("#send").on("click", e => {
+    e.preventDefault();
+    getLatLong($("#direccion").val());
+  });
 
-    $('#cancel').on('click', (e) => {
-        e.preventDefault();
-        clearForm();
-        $("#modal-form")[0].style.display = "none";
-    });
+  $("#cancel").on("click", e => {
+    e.preventDefault();
+    clearForm();
+    $("#modal-form")[0].style.display = "none";
+  });
 
-    $('#show-list').on('click', (e) => {
-        getShops();
-    });
+  $("#show-list").on("click", e => {
+    getShops();
+  });
 
-    $('#show-list').on('click', (e) => {
-        getShops();
-    });
+  $("#show-list").on("click", e => {
+    getShops();
+  });
 
-    $("#searchShop").keyup(function () {
-        //if($(this).val().length >= 3){
-        getShopByName(this, autocomplete);
-        //}
-    });
+  $("#searchShop").keyup(function() {
+    //if($(this).val().length >= 3){
+    getShopByName(this, autocomplete);
+    //}
+  });
 });
 
 function clearForm() {
-    $("#alta-locales-form").find("input, textarea").val("");
-};
+  $("#alta-locales-form")
+    .find("input, textarea")
+    .val("");
+}
 
 function saveShop(lat, lon) {
-    let shop = {
-        name: $("#name").val(),
-        desc: $("#comments").val(),
-        address: $("#direccion").val(),
-        lat: lat,
-        lon: lon,
-        phone: $("#telefono").val(),
-        type: $("#dropdown").val(),
-        hours: $("#horario").val(),
-        img: $("#file").val()
-    }
+  let shop = {
+    name: $("#name").val(),
+    desc: $("#comments").val(),
+    address: $("#direccion").val(),
+    lat: lat,
+    lon: lon,
+    phone: $("#telefono").val(),
+    type: $("#dropdown").val(),
+    hours: $("#horario").val(),
+    img: $("#file").val()
+  };
 
-    $.post(server, {
-        name: shop.name,
-        description: shop.desc,
-        address: shop.address,
-        lat: shop.lat,
-        lon: shop.lon,
-        phone: shop.phone,
-        type: shop.type,
-        hours: shop.hours,
-        img: shop.img
-    }, (data, status) => {
-        console.log(`Status: ${status}`);
-        if (status === 'success') {
-            alert(`El local ${name} fue dado de alta con exito!`);
-            clearForm();
-        }
-    });
-};
+  $.post(
+    server,
+    {
+      name: shop.name,
+      description: shop.desc,
+      address: shop.address,
+      lat: shop.lat,
+      lon: shop.lon,
+      phone: shop.phone,
+      type: shop.type,
+      hours: shop.hours,
+      img: shop.img
+    },
+    (data, status) => {
+      console.log(`Status: ${status}`);
+      if (status === "success") {
+        alert(`El local ${name} fue dado de alta con exito!`);
+        clearForm();
+      }
+    }
+  );
+}
 
 autocomplete = (element, data) => {
+  let matchs = [];
 
-    let matchs = [];
+  data.map(element => {
+    matchs.push({ id: element.id, name: element.name });
+  });
+  console.log(matchs)
 
-    data.map(element => {
-        matchs.push({id: element.id, name: element.name})});
-
-
-    $( function() {
-        $("#searchShop").autocomplete({
-          source: matchs.name
-        });
-      } );
-
-}
+//   $(function() {
+//     $("#searchShop").autocomplete({
+//       source: matchs.name
+//     });
+//   });
+};
